0fd29ce4b0143212a0f4afcb869daa6ef4417285
Martin Štiglinc <martin.stiglinc@gmail.com>
2023-12-10 23:57:38 +0100

    Update day7.mjs
    Added comments to code

--- day7.mjs
+++ day7.mjs
@@ -1,11 +1,15 @@
+//I am so proud of this my own code which take me almost 5 hours to code but it works
+
 import { readFileSync } from "node:fs";
 
-const input = readFileSync("day7.txt", { encoding: "utf-8" }) // read day??.txt content
+//create array of arrays from input txt file
+const input = readFileSync("day7.txt", { encoding: "utf-8" }) // read day7.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
-  .split("\n")
-  .map((item) => item.split(" ")).map(item => [item[0].split(""), item[1]])
+  .split("\n") // split on lines in text
+  .map((item) => item.split(" ")).map(item => [item[0].split(""), item[1]]) //final array of arrays
 
+  //numeric values of cards to now the order from weaker to stronger
   const cardsValue = {
     'A': 14,
     'K': 13,
@@ -22,13 +26,15 @@ const input = readFileSync("day7.txt", { encoding: "utf-8" }) // read day??.txt 
     '2': 2
   }
 
+  //function for part one of day7
   const part1 = () => {
-    
+    //creating helping object for later sorting
     input.map(item => {
       const counts = {}
       item[0].forEach(function (x) { counts[x] = (counts[x] || 0) + 1; })
       let points = 0
 
+      //all possible cards on hand with the points needed to later sorting
       switch(Object.keys(counts).length) {
         case 1:
           points +=70000
@@ -71,14 +77,15 @@ const input = readFileSync("day7.txt", { encoding: "utf-8" }) // read day??.txt 
       }
       item.push(points)
     })
-
+    // sort the array from first card to last card
     input.sort(([[a,b,c,d,e],f,g],[[i,j,k,l,m],n,o])=>
     cardsValue[a]-cardsValue[i] || cardsValue[b] - cardsValue[j] || cardsValue[c]-cardsValue[k] || cardsValue[d] - cardsValue[l] || cardsValue[e] - cardsValue[m])
-         .map((item, index) => item[2] += index+1)
-    input.sort(([a,b,c],[d,e,f])=> c - f)
-  
+         .map((item, index) => item[2] += index+1) //adding index value to the points
+    input.sort(([a,b,c],[d,e,f])=> c - f) //final sorting
+
+    //calculate the final result number for puzzle par one
   let result = input.map((item,index)=> (index+1)*Number(item[1]))
                     .reduce((acc,numb)=>acc+numb,0)
-  console.log(result)
+  console.log(result) //console log the final number
   }
-  part1()
+  part1() //activate function for part one
